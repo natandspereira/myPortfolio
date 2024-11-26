facb0f46807698be74c11dd6e3e2621470461023
alunosuper <alunosuper@SENAI.LABORATORIOS>
2024-11-26 17:14:25 -0300

    Criando Home - Corrigindo

--- src/components/Header/header.php
+++ src/components/Header/header.php
@@ -1,2 +1,17 @@
 <?php
-    echo 'Header';
+    echo ' 
+    <ul class="nav bg-dark p-2">
+        <li class="nav-item">
+            <a class="nav-link active text-white" aria-current="page" href="#">Natan</a>
+        </li>
+        <li class="nav-item ms-auto">
+            <a class="nav-link text-white" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link text-white" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+    </ul>
+';
--- src/pages/Home/home.php
+++ src/pages/Home/home.php
@@ -1,2 +1,9 @@
 <?php 
-    echo 'Home';
+    echo '
+        <div class=\'container ms-5\'>
+            <p class=\'text-white w-50 mt-5 ms-5\'> 
+                Meu nome é Natan, sou programador e desenvolvedor freelancer especializado em criar soluções personalizadas para empresas e empreendedores. 
+                Com experiência em diversas tecnologias e frameworks, estou aqui para ajudar você a transformar suas ideias em realidade digital.
+            </p>
+        </div>
+    ';
